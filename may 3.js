284dcac62b398b469c083facd4b17afab6240e81
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-05-03 12:21:40 +0200

    Add files via upload

---
+++ may 3.js
@@ -0,0 +1,12 @@
+//Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+function shortcut (str) {
+    shortcut = str.replace(/[aeiou]/g, '')
+    return shortcut
+}
+
