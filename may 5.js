9b82c25d8b05033b6939e6ba366522ef18f02f4c
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-05-06 01:32:50 +0200

    Add files via upload

---
+++ may 5.js
@@ -0,0 +1,17 @@
+// Create a combat function that takes the player's current health 
+//and the amount of damage recieved, 
+// and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+    return health < damage ? 0 : health - damage
+  }
+
+  function combat(health, damage) {
+    if (health - damage < 0){
+      return 0;
+    }else{
+      return health - damage;
+    }
+  }
+
+
