6d89c379f47ecf614b9e950a62fe31d45ef9c760
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-05-06 11:49:09 +0200

    Add files via upload

---
+++ may 6.js
@@ -0,0 +1,8 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+// function noSpace(x){
+//     let newStr = x.split(' ').join('');
+//         return newStr
+// }
+
+const noSpace = (x) => x.split(' ').join('');
