68d44655838476f9f0811d9b0861fed878a253e4
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-05-07 13:19:00 +0200

    Add files via upload

---
+++ may 7.js
@@ -0,0 +1,11 @@
+//Complete the solution so that it returns true if the first argument(string) 
+//passed in ends with the 2nd argument (also a string).
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// function solution(str, ending){
+//     return str.endsWith(ending)
+// }
+
+const solution = (a,b) => a.endsWith(b);
